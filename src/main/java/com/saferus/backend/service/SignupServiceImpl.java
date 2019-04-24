5d68dad2937fa474581978d9660f12b03a242c6c
DAIPL3G1 <48056140+DAIPL3G1@users.noreply.github.com>
2019-04-24 13:51:45 +0100

    Make it better

--- src/main/java/com/saferus/backend/service/SignupServiceImpl.java
+++ src/main/java/com/saferus/backend/service/SignupServiceImpl.java
@@ -29,9 +29,6 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 public class SignupServiceImpl implements SignupService {
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
     private UserRepository userRepository;
 
     @Autowired
