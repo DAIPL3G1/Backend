2f880f095d827c45d1197cff533aa97afe837742
DAIPL3G1 <48056140+DAIPL3G1@users.noreply.github.com>
2019-04-24 13:47:34 +0100

    Make it better

--- src/main/java/com/saferus/backend/config/WebSecurityConfig.java
+++ src/main/java/com/saferus/backend/config/WebSecurityConfig.java
@@ -13,7 +13,6 @@ import com.saferus.backend.security.jwt.JwtAuthEntryPoint;
 import com.saferus.backend.security.jwt.JwtAuthTokenFilter;
 import com.saferus.backend.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,13 +21,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
